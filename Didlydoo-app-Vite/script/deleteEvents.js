ceb1f655391e0bde62efac4283522fdd4296d864
jeremy frederic <jeremyfrederic12@gmail.com>
2024-01-23 13:54:03 +0100

    fini java

--- Didlydoo-app-Vite/script/deleteEvents.js
+++ Didlydoo-app-Vite/script/deleteEvents.js
@@ -1,9 +1,8 @@
-export async function deleteEvent(id) {
+export async function deleteEvent() {
+    //element à automatiser par après
+        let id = "460fefc37b98";
 
     await fetch(`http://localhost:3000/api/events/${id}/`, {
         method: "DELETE"
     })
-        .then(res => res.json())
-        .then(res => console.log('res : ', res));
-
 }
